8832c0b138a054d6681c0e9c304234a55254fb90
Rohit Sharma <rohit.sharma@aliando.com>
2026-07-01 00:53:12 +0530

    added test-rd

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg" {
   name     = "demo-rg"
   location = "Central India"
 }
+
+resource "azurerm_resource_group" "rg" {
+  name     = "test-rg"
+  location = "Central India"
+}
